73129837fca63efa3843813e30198a4747644641
Martin Nowak <code@dawg.eu>
2017-03-04 17:07:43 +0100

    add delete label

---
+++ delete_label.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ueo pipefail
+
+gh_token="$1"; shift
+label="$1"; shift
+
+msg='{"name": "'$label'"}'
+echo "$msg"
+echo "Does this look correct?"
+select yn in "yes" "no"; do
+    case $yn in
+        yes ) break;;
+        no ) exit 1;;
+    esac
+done
+
+for proj in dmd druntime phobos tools installer dlang.org; do
+    url=$(curl -fsSL -H "Authorization: token $gh_token" "https://api.github.com/repos/dlang/$proj/labels" \
+                 | jq -r ".[] | select(.name == \"$label\") | .url")
+    if [ ! -z "$url" ]; then
+        curl -fsSL -X DELETE -H "Authorization: token $gh_token" $url \
+             --data "$msg" | jq --raw-output '.url'
+    fi
+done
